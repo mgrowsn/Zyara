f81eeb0b54852357ad16944b3116d4ae491fc118
Mehak <mehakthamen@gmail.com>
2025-06-06 18:31:41 +0530

    Initial Commit

---
+++ src/main/java/com/Zyara/ZyaraApplication.java
@@ -0,0 +1,13 @@
+package com.Zyara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zyara/ZyaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zyara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
